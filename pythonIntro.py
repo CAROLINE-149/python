6e86ba6be10f8cfc1a51a20593f70d254363ec2c
CAROLINE <caroonyango9@gmail.com>
2025-12-30 19:06:55 +0300

    Learning python

---
+++ pythonIntro.py
@@ -0,0 +1,37 @@
+"""
+Python- Is a popular programming language created by Guido van Rossum and first released in 1991.
+It is known for its readability, simplicity, and versatility, making it a great choice for beginners and experienced developers alike.
+It is used for;
+- Web Development (using frameworks like Django and Flask)
+- Data Science and Machine Learning (using libraries like Pandas, NumPy, and TensorFlow
+-software Development
+- Automation and Scripting
+- Game Development (using libraries like Pygame)
+- Network Programming
+- Desktop Application Development (using frameworks like Tkinter and PyQt)
+- Cybersecurity
+- Internet of Things (IoT)
+- Scientific Computing
+- Artificial Intelligence
+- DevOps
+- Cloud Computing
+- Mobile App Development (using frameworks like Kivy and BeeWare)
+- Blockchain Development
+- Augmented Reality (AR) and Virtual Reality (VR)
+- Robotics etc.
+ 
+WHAT PYTHON CAN DO?
+Python can be used on a server to create web applications.
+Python can be used alongside software to create workflows.
+Python can connect to database systems. It can also read and modify files.
+Python can be used to handle big data and perform complex mathematics.
+Python can be used for rapid prototyping, or for production-ready software development.
+
+"""
+#print("Hello World!")
+
+"""
+#checking python version 
+import sys
+print(sys.version)
+"""
---
+++ pythonSyntax.py
@@ -0,0 +1,47 @@
+"""
+PYTHON INDENTATION
+Indentation is the spaces at the beginning of a codeline.
+Where in other programming languages the indentation in code is for readability only, the indentation in Python is very important.
+It is used to define a block of code.
+"""
+"""
+#Example
+if 5>2:
+    print("5 is greater then 2")  #Indented line
+"""
+
+#PYTHON VARIABLES
+# - A variable is a container for storing data values that can change during the execution of a program.
+# - In python variables are created when you assign a value to it.
+# - Python has no command for declaring a variable.
+# - A variable can hold different data types such as integers, floats, strings, lists, tuples, dictionaries, etc.
+# - Variable names must start with a letter or the underscore character.
+# - A variable name cannot start with a number.
+# - Variable names are case-sensitive (age, Age and AGE are three different variables).
+# - Variable names should not be a reserved word in Python (like keywords: if, else, while, for, etc.)
+# - You can assign values to multiple variables in one line.
+"""
+#Example
+x=5
+z="Caroline"
+print(x)
+print(z)
+"""
+"""
+PYTHON STATEMENTS
+Statements are instructions that a Python interpreter can execute.
+In python a statement ususally ends when the line ends. However, in some cases, a statement can span multiple lines.
+
+"""
+#Example
+#print("Python is fun!")
+
+"""
+MANY STATEMENTS
+In Python, you can write multiple statements in a single line by separating them with a semicolon (;).
+"""
+#Example
+#print("Hello"); print("World");print("!");print("Welcome to Python")
+
+#Best practice is to write one statement per line for better readability.
+
